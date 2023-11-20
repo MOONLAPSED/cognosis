258e40537256249b40b32632ae0a5a9cfb496473
MOONLAPSED <137737122+MOONLAPSED@users.noreply.github.com>
2023-11-19 22:31:54 -0600

    Update main.py ==fixed sweep's (doubled) code

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import http.server
 import socketserver
-import os, sys, json, argparse, logging, unittest
+import os
+import sys
+import argparse
+import logging
+import unittest
+
 from src.utils.errors import *  # Import the error classes
 from src.utils.logutils import *  # Check and create the log directory
 from src.api.api import *  # Import the api module and its classes
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 log_directory = 'logs'
 log_file_path = os.path.join(log_directory, 'app.log')
@@ -11,76 +17,44 @@ log_file_path = os.path.join(log_directory, 'app.log')
 logger = init_logging(log_directory, log_file_path)
 
 
-
-def main():
-    try:
-        prompt = []
-        parser = argparse.ArgumentParser(description='cognosis by MOONLAPSED@gmail.com MIT License')
-        parser.add_argument('prompt', nargs='*', help='Enter the prompt here')
-        args = parser.parse_args()
-        prompt.extend(args.prompt)
-        if len(prompt) == 0:
-            prompt.append('Hello world!')
-        else:
-            for line in args.prompt:
-                prompt.append(line)
-                prompt.append('\n')  # Append newline after each line
-        if prompt[-1] == '\n':  # Remove the last newline if present
-            prompt.pop()
-        prompt = ''.join(prompt)
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-        print(prompt)
-        api_instance = API()
-        api_instance.call_api(prompt)
-        test_suite = unittest.defaultTestLoader.discover(start_dir='.', pattern='test_*.py')
-        unittest.TextTestRunner().run(test_suite)
-        logging.error(f"An error occurred: {e}")
-        print(prompt)
-        test_suite = unittest.defaultTestLoader.discover(start_dir='.', pattern='test_*.py')
-        unittest.TextTestRunner().run(test_suite)
-    test_suite = unittest.defaultTestLoader.discover(start_dir='.', pattern='test_*.py')
-    unittest.TextTestRunner().run(test_suite)
 def main():
     """
     Main function of the program.
     Parses command line arguments and executes the program accordingly.
     """
     try:
-        prompt = []
         parser = argparse.ArgumentParser(description='cognosis by MOONLAPSED@gmail.com MIT License')
         parser.add_argument('prompt', nargs='*', help='Enter the prompt here')
         args = parser.parse_args()
-        prompt.extend(args.prompt)
-        if len(prompt) == 0:
-            prompt.append('Hello world!')
-        else:
-            for line in args.prompt:
-                prompt.append(line)
-                prompt.append('\n')  # Append newline after each line
-        if prompt[-1] == '\n':  # Remove the last newline if present
-            prompt.pop()
-        prompt = ''.join(prompt)
+
+        if not args.prompt:  # If prompt is empty, provide a default
+            args.prompt.append('Hello world!')
+
+        # Construct the prompt as a single string
+        prompt = ' '.join(args.prompt)
+
+        # Remove the last newline if present
+        if prompt.endswith('\n'):
+            prompt = prompt[:-1]
     except Exception as e:
-        logging.error(f"An error occurred: {e}")
-    print(prompt)
+        logger.error(f"An error occurred: {e}")
+        sys.exit(1)
+
+    logger.info(f"Prompt: {prompt}")
+
+    # Run tests
     test_suite = unittest.defaultTestLoader.discover(start_dir='.', pattern='test_*.py')
-    unittest.TextTestRunner().run(test_suite)
+    result = unittest.TextTestRunner().run(test_suite)
 
-# TODO ~/static/.server/statserv.py integration      
-"""
-class MyHandler(http.server.SimpleHTTPRequestHandler):
-    def do_GET(self):
-        if self.path.endswith(".html"):
-            self.send_response(200)
-            self.send_header("Content-type", "text/html")
-            self.end_headers()
-            with open(self.path[1:], "r") as f:
-                self.copyfile(f, self.wfile)
-        else:
-            super().do_GET()
+    if result.wasSuccessful():
+        logger.info("Tests passed successfully.")
+    else:
+        logger.error("Some tests failed.")
 
-port = 8000
-server = socketserver.TCPServer(("localhost", port), MyHandler)
-server.serve_forever()
-"""
+    # Instantiate API and call the API with the prompt
+    api_instance = API()
+    api_instance.call_api(prompt)
+
+
+if __name__ == "__main__":
+    main()
