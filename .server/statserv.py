52f5db9abfa821be3a40eed8f3e9f262adae1791
MOONLAPSED <137737122+MOONLAPSED@users.noreply.github.com>
2023-11-20 16:54:31 -0600

    Update statserv.py

--- .server/statserv.py
+++ .server/statserv.py
@@ -13,41 +13,17 @@ with socketserver.TCPServer(("", PORT), handler) as httpd:
     print(f"Serving static files from {DIRECTORY} on port {PORT}")
     httpd.serve_forever()
 
-    def help(self):
-        """
-        Print available commands:
-        - start: start server
-        - stop: stop server
-        - restart: restart server
-        - status: status server
-        - help: help
-        - exit: exit
-        """
-        print("start - start server")
-        print("stop - stop server")
-        print("restart - restart server")
-        print("status - status server")
-
-        def help(self):
-            """
-            Print available commands:
-            - start: start server
-            - stop: stop server
-            - restart: restart server
-            - status: status server
-            - help: help
-            - exit: exit
-            """
-            print("start - start server")
-            print("stop - stop server")
-            print("restart - restart server")
-            print("status - status server")
-            print("help - help")
-            print("exit - exit")
-
+def help(self):
     """
-    This will start a simple HTTP server on port 8080. You can then access your static files by opening a web browser and navigating to http://localhost:8080/
-    To access the index.html file, you would go to http://localhost:8080/index.html.
+    Print available commands:
+    - start: start server
+    - stop: stop server
+    - restart: restart server
+    - status: status server
+    - help: help
+    - exit: exit
     """
-    print(f"Serving static files from {DIRECTORY} on port {PORT}")
-    httpd.serve_forever()
+    print("start - start server")
+    print("stop - stop server")
+    print("restart - restart server")
+    print("status - status server")
