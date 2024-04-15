bf0e663ae328d8cc983ed74d49ca03d5e50ab54c
moonlapsed <MOONLAPSED@gmail.com>
2024-04-14 23:42:09 -0500

    snip

--- src/utils/touch.py
+++ src/utils/touch.py
@@ -32,37 +32,3 @@ if __name__ == "__main__":
 
     file_name, old_content, new_content = sys.argv[1:]
     replace_text_in_file(file_name, old_content, new_content)
-
-
-def test_replace_text_in_file():
-    # Arrange
-    file_name = "test_file.txt"
-    old_content = "Hello"
-    new_content = "Goodbye"
-    expected_content = "Goodbye, world!"
-
-    # Create a test file with initial content
-    with open(file_name, 'w') as file:
-        file.write("Hello, world!")
-
-    # Act
-    replace_text_in_file(file_name, old_content, new_content)
-
-    # Assert
-    with open(file_name, 'r') as file:
-        content = file.read()
-        assert content == expected_content
-
-    # Clean up
-    os.remove(file_name)
-
-
-def test_replace_text_in_file_error():
-    # Arrange
-    file_name = "nonexistent_file.txt"
-    old_content = "Hello"
-    new_content = "Goodbye"
-
-    # Act and Assert
-    with pytest.raises(FileNotFoundError):
-        replace_text_in_file(file_name, old_content, new_content)
