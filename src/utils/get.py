450e73c0de3412dc2f051bd8f23cd10fc2f90067
moonlapsed <MOONLAPSED@gmail.com>
2024-04-16 22:58:15 -0500

    cat utils

--- src/utils/get.py
+++ src/utils/get.py
@@ -24,10 +24,28 @@ def load_file_content(file_name: str, start_line: int = 0, end_line: int = 1000)
         sys.stderr.write(f"Error: {str(e)}\n")
         sys.exit(1)
 
+
+def get_project_tree() -> List[str]:
+    """
+    Get project files by "git ls-files" command
+
+    :return: list of relative file paths of the project
+    """
+    try:
+        output: List[str] = subprocess.check_output(["git", "ls-files"]).decode("utf-8").split("\n")
+        if len(output) > 100:
+            sys.stderr.write("Error: Too many files, you need to view the files and directory structure in each directory through the 'ls' command.\n")
+            sys.exit(1)
+        return output
+    except Exception as e:
+        sys.stderr.write(f"Error: {str(e)}\n")
+
+
 if __name__ == "__main__":
     file_name = sys.argv[1]
     start_line = int(sys.argv[2]) if sys.argv[2] != '${startLine}' else 0
     end_line = int(sys.argv[3]) if sys.argv[3] != '${endLine}' else 1000
 
+    print(get_project_tree())
     print(load_file_content(file_name, start_line, end_line))
     sys.exit(0)
