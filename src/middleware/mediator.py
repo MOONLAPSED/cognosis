ccf39a897d124b659233d9fa7ffa2080f26ae034
moonlapsed <moonlapsed@gmail.com>
2024-06-24 23:47:14 -0500

    /middleware/mediator.py for REST REPL access

---
+++ src/middleware/mediator.py
@@ -0,0 +1,13 @@
+import http.server
+import json
+import sys
+import code
+import io
+
+class REPLProxy(http.server.BaseHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        # ...
+        pass
+    def do_POST(self):
+        # ...
+        pass
